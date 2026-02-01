8465f48891c71ae345a023ecc0b4cecb7aa06f02
Amanda Baker <amandarbaker24@gmail.com>
2026-02-01 00:38:04 -0500

    fix spelling error in UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>QuantumPharm</h1>
+      <h1>Quantum Pharma</h1>
       <p> Click here to see profile settings <a href="/profile/settings">/profile/settings</a></p>
     </main>
   );
